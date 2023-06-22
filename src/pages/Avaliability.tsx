15acaa9d0309d63478bb1a773f1faae526917f29
Teo Turner <teoturner@gmail.com>
2023-06-22 22:51:00 +0100

    install react-datepicker

--- src/pages/Avaliability.tsx
+++ src/pages/Avaliability.tsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import { CenteredContent } from "../components/Layout/CenterContent";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 
 export const Avaliability = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
-    <CenteredContent>
-      <div className="m-4 min-grow">
-        <div>
-          Great so in that timeframe on what days is the first person avaliable?
+    <>
+      <CenteredContent>
+        <div className="m-4 min-grow">
+          <div>
+            Great so in that timeframe on what days is the first person
+            avaliable?
+          </div>
+
+          <div>SELECT DAYS</div>
         </div>
-        <div>SELECT DAYS</div>
-      </div>
-      <Link to="/result">
-        <button>Continue</button>
-      </Link>
-    </CenteredContent>
+        <Link to="/result">
+          <button>Continue</button>
+        </Link>
+      </CenteredContent>
+      <DatePicker
+        selected={startDate}
+        onChange={(date: any) => setStartDate(date)}
+        inline
+      />
+    </>
   );
 };
